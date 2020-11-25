97f248eed190f07ffcfe59cf4f2047b56f94e8c6
Gabriel Pivaro <gabrconc@gmail.com>
2020-11-25 01:20:00 -0600

    Part I completed

--- static/js/app_bonus.js
+++ static/js/app_bonus.js
@@ -21,17 +21,21 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // circle properties
-var circleRadius = 12;
+var circleRadius = 10;
 
 // Define the data columns for the plot
 var xColumn = "poverty";
 var yColumn = "healthcare"
 
+// Axis title offset
+var xAxisLabelOffset = 20;
+var yAxisLabelOffset = 35;
+
 // Use d3 to import the data
 d3.csv("data/data.csv").then(function (data) {
 
     // Print the csv imported data
-    console.log(data);
+    // console.log(data);
 
     // Cast the data value to a number for each piece of data
     data.forEach(function (data) {
@@ -89,136 +93,27 @@ d3.csv("data/data.csv").then(function (data) {
         .classed("stateText", true)
         .attr("text-anchor", "middle")
         .attr("x", d => xScale(d[xColumn]))
-        .attr("y", d => yScale(d[yColumn]))
+        .attr("y", d => yScale(d[yColumn]) + 3)
         .text(d => `${d.abbr}`)
         .attr("font-size", `${circleRadius}px`);
+
+    // Append x-axis titles
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top + xAxisLabelOffset})`)
+        .attr("text-anchor", "middle")
+        .classed("dow-text text", true)
+        .text("In Poverty (%)");
+
+    // Append y-axis titles
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - yAxisLabelOffset)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("text-anchor", "middle")
+        .classed("dow-text text", true)
+        .text("Lacks Healthcare (%)");
 
 
 }).catch(function (error) {
     console.log(error);
 });
-
-
-
-
-
-
-
-
-
-
-// // Circle properties
-// var circleRadius = 12;
-// var xCircleLabelOffset = circleRadius / 1.5;
-// var yCircleLabelOffset = circleRadius / 4;
-
-// // Define the data columns for the plot
-// var xColumn = "poverty";
-// var yColumn = "healthcare"
-
-// // Axis properties
-// var xAxisLabelText = "In Poverty (%)";
-// var xAxisLabelOffset = 55;
-// var yAxisLabelText = "Lack Healthcare (%)";
-// var yAxisLabelOffset = 45;
-
-
-
-
-
-// // Set the linear scale of x and y
-// // var xScale = d3.scale.linear().range([0, chartWidth]);
-// var xScale = d3.scaleLinear().range([0, chartWidth]);
-// // Invert the y axis
-// // var yScale = d3.scale.linear().range([chartHeight, 0]);
-// var yScale = d3.scaleLinear().range([chart
-// Height, 0]);
-
-// // Define the xaxis property
-// var xAxisG = g.append("g")
-//     .attr("class", "x axis")
-//     .attr("transform", "translate(0," + chart
-// Height + ")");
-
-// // Define the yaxis property
-// var yAxisG = g.append("g")
-//     .attr("class", "y axis");
-
-// // Add xaxis labels
-// var xAxisLabel = xAxisG.append("text")
-//     .style("text-anchor", "middle")
-//     .attr("transform", "translate(" + (chartWidth / 2) + "," + xAxisLabelOffset + ")")
-//     .attr("class", "label")
-//     .text(xAxisLabelText);
-
-// // Add yaxis labels
-// var yAxisLabel = yAxisG.append("text")
-//     .style("text-anchor", "middle")
-//     .attr("transform", "rotate(-90)")
-//     .attr("y", 0 - yAxisLabelOffset)
-//     .attr("x", 0 - (chart
-//     Height / 2))
-//     .attr("class", "label")
-//     .text(yAxisLabelText);
-
-// // Define the axis scale
-// // var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-// var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-// // var yAxis = d3.svg.axis().scale(yScale).orient("left");
-// var yAxis = d3.svg.axis().scale(yScale).orient("left");
-
-// // Render the plot on the SVG element
-// function render(data) {
-
-//     // Set the domain scale to be the min and max of the x and y scales
-//     xScale.domain(d3.extent(data, function (d) { return d[xColumn]; }));
-//     yScale.domain(d3.extent(data, function (d) { return d[yColumn]; }));
-
-
-//     // Apply the axis
-//     xAxisG.call(xAxis);
-//     yAxisG.call(yAxis);
-
-//     // Bind data to the circles
-//     var circles = g.selectAll("circle").data(data);
-
-//     // Enter data
-//     circles.enter()
-//         .append("circle")
-//         .attr("r", circleRadius)
-//         .classed("stateCircle", true)
-
-//     // Update data
-//     circles
-//         .attr("cx", function (d) { return xScale(d[xColumn]); })
-//         .attr("cy", function (d) { return yScale(d[yColumn]); });
-
-
-//     // Exit data
-//     circles.exit().remove();
-
-
-//     //Add the SVG Text Element to the group
-//     var text = g.selectAll("circles")
-//         .data(data)
-//         .enter()
-//         .append("text");
-
-//     //Add SVG Text Element Attributes
-//     var textLabels = text
-//         .attr("x", function (d) { return (xScale(d[xColumn]) - xCircleLabelOffset); })
-//         .attr("y", function (d) { return (yScale(d[yColumn]) + yCircleLabelOffset); })
-//         .text(function (d) { return d['abbr']; })
-//         .classed("stateText", true)
-//         .attr("font-size", `${circleRadius}px`);
-
-
-
-
-
-
-
-
-
-
-
